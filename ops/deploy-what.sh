462b0f14036b5ef2b17dc5ddb69f54075b7241c5
Josh Bleecher Snyder <josharian@gmail.com>
2025-09-26 14:29:53 -0700

    ops: fix endpoint for deploy-what.sh

--- ops/deploy-what.sh
+++ ops/deploy-what.sh
@@ -2,7 +2,7 @@
 
 echo "🕸️  checking what is deployed..."
 
-DEPLOYED_SHA=$(curl -s https://exe.dev/debug/gitsha)
+DEPLOYED_SHA=$(curl -s https://exed-01.crocodile-vector.ts.net/debug/gitsha)
 
 if [ $? -ne 0 ]; then
 	echo "😞 could not get deployed SHA (curl failed)"
