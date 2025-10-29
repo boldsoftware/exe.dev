21a95f22a667508f67bcd1eb1e4e58c2b32b5cd0
David Crawshaw <david@zentus.com>
2025-10-29 08:22:23 -0700

    ops: enable sshpiper cert advertising if a cert exists

--- sshpiper.sh
+++ sshpiper.sh
@@ -20,9 +20,10 @@ if [ ! -f sshpiper/metrics ]; then
     cd sshpiper && go build -o metrics ./plugin/metrics && cd ..
 fi
 
-# Get private key from database
-PRIVATE_KEY=$(sqlite3 exe.db "SELECT private_key FROM ssh_host_key WHERE id = 1;")
-[ -z "$PRIVATE_KEY" ] && { echo "No SSH host key found"; exit 1; }
+# Get private key (and optional cert) from database
+HOST_PRIVATE_KEY=$(sqlite3 exe.db "SELECT private_key FROM ssh_host_key WHERE id = 1;")
+[ -z "$HOST_PRIVATE_KEY" ] && { echo "No SSH host key found"; exit 1; }
+HOST_CERT_SIG=$(sqlite3 exe.db "SELECT cert_sig FROM ssh_host_key WHERE id = 1;")
 
 # Wait until something is listening on the piper plugin port
 echo "Waiting for service on port $PIPER_PLUGIN_PORT..."
@@ -51,11 +52,18 @@ fi
 
 # Start sshpiper
 cd ./sshpiper && go build -o sshpiperd ./cmd/sshpiperd; cd ..
+
+HOST_CERT_ARGS=()
+if [ -n "$HOST_CERT_SIG" ]; then
+    HOST_CERT_ARGS+=(--server-cert-data="$(printf '%s' "$HOST_CERT_SIG" | base64 -w 0)")
+fi
+
 exec ./sshpiper/sshpiperd \
     --log-level=DEBUG \
     --drop-hostkeys-message \
     --port=2222 \
     --address=0.0.0.0 \
-    --server-key-data="$(echo "$PRIVATE_KEY" | base64 -w 0)" \
+    --server-key-data="$(printf '%s' "$HOST_PRIVATE_KEY" | base64 -w 0)" \
+    "${HOST_CERT_ARGS[@]}" \
     grpc --endpoint=localhost:$PIPER_PLUGIN_PORT --insecure \
     "${METRICS_ARGS[@]}"
