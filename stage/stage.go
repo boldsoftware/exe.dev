2f56f58021463482e18447621162ffd8a2014cf2
Josh Bleecher Snyder <josharian@gmail.com>
2025-11-21 08:49:34 -0800

    stage: consolidate to a single file
    It was silly to split it into two.

--- stage/stage.go
+++ stage/stage.go
@@ -24,3 +24,77 @@ func (e Env) String() string {
 	}
 	return e.Name
 }
+
+func Local() Env {
+	return Env{
+		Name: "local",
+
+		WebHost:  "localhost",
+		ReplHost: "localhost",
+		BoxHost:  "localhost",
+
+		// auto-start cobble/pebble for ACME testing
+		UseRoute53:        false,
+		UseCobble:         true,
+		DiscoverPublicIPs: false,
+
+		ReplDev: true,
+
+		DevMode: "local",
+	}
+}
+
+func Test() Env {
+	return Env{
+		Name: "test",
+
+		WebHost:  "localhost",
+		ReplHost: "localhost",
+		BoxHost:  "localhost",
+
+		// tests start their own cobble/pebble instances as needed
+		UseRoute53:        false,
+		UseCobble:         false,
+		DiscoverPublicIPs: false,
+
+		ReplDev: false,
+
+		DevMode: "test",
+	}
+}
+
+func Staging() Env {
+	return Env{
+		Name: "staging",
+
+		WebHost:  "exe-staging.dev",
+		ReplHost: "exe-staging.dev",
+		BoxHost:  "exe-staging.dev",
+
+		UseRoute53:        true,
+		UseCobble:         false,
+		DiscoverPublicIPs: true,
+
+		ReplDev: false,
+
+		DevMode: "",
+	}
+}
+
+func Prod() Env {
+	return Env{
+		Name: "prod",
+
+		WebHost:  "exe.dev",
+		ReplHost: "exe.dev",
+		BoxHost:  "exe.dev",
+
+		UseRoute53:        true,
+		UseCobble:         false,
+		DiscoverPublicIPs: true,
+
+		ReplDev: false,
+
+		DevMode: "",
+	}
+}
