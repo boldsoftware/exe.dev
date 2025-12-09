e9d794f3a79820586e7c828e2f649f5a459d0191
Josh Bleecher Snyder <josharian@gmail.com>
2025-12-09 15:01:59 -0800

    ops: give CI VM host more RAM
    Boxes ask for 8gb each, as of d011b77782f70c200b6df64f00c94acc116ff731.
    Maybe bumping this up in turn will help.
    That was an 8x increase; this is a 4x increase.
    Still, hope springs eternal.

--- ops/ci-vm-start.sh
+++ ops/ci-vm-start.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 NAME="${NAME:-ci-ubuntu-$(date +%Y%m%d%H%M%S)}"
 VCPUS="${VCPUS:-4}"
-RAM_MB="${RAM_MB:-4096}"           # 4GiB
+RAM_MB="${RAM_MB:-16384}"          # 16GiB
 DISK_GB="${DISK_GB:-40}"           # thin-provisioned
 DATA_DISK_GB="${DATA_DISK_GB:-50}" # ZFS data disk
 BASE_IMG="${BASE_IMG:-/var/lib/libvirt/images/ubuntu-24.04-base.qcow2}"
