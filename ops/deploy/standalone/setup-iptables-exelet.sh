880784263568b2d5b639a0a8b48b701839087843
Evan Hazlett <evan@h5t.io>
2026-03-25 16:36:04 -0400

    ops: remove ssh from iptables

--- ops/deploy/standalone/setup-iptables-exelet.sh
+++ ops/deploy/standalone/setup-iptables-exelet.sh
@@ -5,10 +5,10 @@ IFACE=tailscale0
 iptables -F INPUT
 
 # allow loopback and tailscale; drop everything else
+iptables -A INPUT -j ts-input
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -i "$IFACE" -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -j DROP
 
 # outbound stays allowed
